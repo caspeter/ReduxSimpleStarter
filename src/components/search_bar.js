2279f2048cdcb602b586ae6f431af6ab86a27a5c
Cassie Peterson <cas.j.peters@gmail.com>
2017-01-21 20:07:44 -0700

    finish first react app

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -18,17 +18,19 @@ class SearchBar extends Component {
     }
 
     render() {
-        return ( < div >
+        return (
+          <div className="search-bar">
             <input
             value = {this.state.term}
-            onChange = {event => this.setState({ term: event.target.value }) }/>
+            onChange = {event => this.onInputChange( event.target.value)} />
             </div>
         );
     }
 
-    // onInputChange (event) {
-    //   console.log(event.target.value);
-    // }
+    onInputChange (term) {
+      this.setState({term});
+      this.props.onSearchTermChange(term);
+    };
 }
 
 export default SearchBar;
