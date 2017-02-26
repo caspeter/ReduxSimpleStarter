46b9b1bbdac8eb31ffc32182388ba3f2ea7e1f09
Cassie Peterson <cas.j.peters@gmail.com>
2017-02-26 10:46:42 -0700

    refactor chart code to a component for reuse

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,19 +1,22 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import Chart from '../components/chart.js';
 
 class WeatherList extends Component {
     renderWeather(cityData) {
+      console.log(cityData);
       const name = cityData.city.name;
       const temps = cityData.list.map(weather => weather.main.temp);
+      // const pressure = cityData.list.map(weather => weather.main.pressure);
         return (
             <tr key={name}>
               <td>{name}</td>
               <td>
-                <Sparklines height={120} width={180} data={temps}>
-                  <SparklinesLine color="red" />
-                </Sparklines>
+                <Chart data={temps} color="orange" />
               </td>
+              {/* <td>
+                <Chart data={pressure} color="green" />
+              </td> */}
             </tr>
         );
     };
