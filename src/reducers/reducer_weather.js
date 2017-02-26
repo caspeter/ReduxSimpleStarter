b284de792cdddb6875ec75b29296b527c371aab5
Cassie Peterson <cas.j.peters@gmail.com>
2017-02-26 10:39:08 -0700

    Chart for temp works and displays

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -3,7 +3,7 @@ import { FETCH_WEATHER } from '../actions/index.js'
 export default function (state = [], action){
   switch (action.type) {
     case FETCH_WEATHER:
-      return [ action.payload.data, ...state ]; //similar to: return state.concat([action.payload.data])
+      return [ action.payload.data, ...state ]; //ES6 for: return state.concat([action.payload.data])
       // returns a new state instead of mutating state. mutating state won't work
 
   }
